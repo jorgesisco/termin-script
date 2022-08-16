6685fa133e5bb8edd16f592688676c47b5ffcd78
Jorge Sisco <jorgesisco17@gmail.com>
2022-08-16 06:49:58 +0200

    almost done

--- script.py
+++ script.py
@@ -1,20 +1,24 @@
+
+
 from dotenv import load_dotenv
 load_dotenv()
+
 
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
+from selenium.webdriver.support.select import Select
 
 driver = webdriver.Chrome()
 
 #Funtion to Fill info for changing Termin!
 def change_termin():
     driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen?lang=en")
+    time.sleep(3)
+    driver.find_element(By.LINK_TEXT,"Delete Appointment").click()
     time.sleep(4)
-    driver.find_element(By.LINK_TEXT,"Change Appointment").click()
-    time.sleep(10)
     driver.find_element(By.ID,"xi-tf-948").send_keys(os.environ.get("FULL_NAME"))
     driver.find_element(By.ID,"xi-tf-949").send_keys(os.environ.get("LAST_NAMES"))
     driver.find_element(By.ID,"xi-tf-951").send_keys(os.environ.get("PROCESS_NUM"))
@@ -22,18 +26,31 @@ def change_termin():
     driver.find_element(By.ID,"xi-tf-950").send_keys(os.environ.get("BD"))
     driver.find_element(By.ID,"applicationForm:managedForm:proceed").click()
 
-    for i in range(0, 20):
-         time.sleep(15)
-         driver.refresh()
-         print(f'Refresh #{i+1}')
+
+def get_termin():
+    driver.get("https://otv.verwalt-berlin.de/ams/TerminBuchen?lang=en")
+    time.sleep(1)
+    driver.find_element(By.LINK_TEXT,"Book Appointment").click()
+    time.sleep(2)
+    driver.find_element(By.ID,"xi-cb-1").click()
+    driver.find_element(By.ID,"applicationForm:managedForm:proceed").click()
+    time.sleep(3)
+    # driver.find_element(By.ID,"xi-sel-400").click()
+    driver.find_element(By.ID, "xi-sel-400").click()
+    time.sleep(3)
+    driver.find_elements(By.LINK_TEXT,'Albania')
+
 
     
+
+
+
 
     
 
 
 change_termin()
-
+# get_termin()
 
 
 
@@ -41,6 +58,7 @@ change_termin()
 
 
 # conda activate env2
-#python -m pip install selenium
+#python3 -m pip install selenium
 #pip install os
-# python -m pip install python-dotenv
+# python3 -m pip install python-dotenv
+# or conda install -c conda-forge python-dotenv
